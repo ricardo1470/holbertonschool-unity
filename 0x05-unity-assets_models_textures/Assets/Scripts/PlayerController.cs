912a2dd39ecf2bc0d5230937a19600a8eb0f6fba
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2021-02-03 05:21:24 -0500

    position tje main camera at an offset behind the player

--- 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
+++ 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -9,40 +9,111 @@ public class PlayerController : MonoBehaviour
     // variables for x-axis and y-axis
     // x = horizontal
     private float Horizontal;
+
     // z = vertical
     private float Vertical;
+
+    // player object assignment
+    private CharacterController player;
+
     // speed
     public float speed;
 
+    //direction the player is moving
+    private Vector3 movePlayer;
+
+    // camera
+    public Camera mainCamera;
+
+    //camera forward
+    private Vector3 camForward;
+
+    // camera to the right
+    private Vector3 camRight;
+
+    // gravity
+    private float gravity = 9.8f;
+
+    // falling speed
+    public float fallVelocity;
+
     // jump
-    public float jump;
+    public float jumpForce;
 
-    // gravity Value
-    private float gravityValue = 9.81f;
-
-    private UnityEngine.CharacterController player;
-
-    void Start ()
+    public void Start ()
     {
         player = GetComponent<CharacterController> ();
     }
 
-    void update ()
+    public void update ()
     {
     }
 
-
-    void FixedUpdate()
+    public void FixedUpdate()
     {
         Horizontal = Input.GetAxis("Horizontal");
         Vertical = Input.GetAxis("Vertical");
-        Vector3 move = new Vector3(Horizontal, 0.0f, Vertical);
-        player.Move(move * speed * Time.deltaTime);
 
-        // value keyboard Space
-        if (Input.GetKey(KeyCode.Space))
+        //camera directtion
+        camDirection();
+
+        movePlayer = ((Horizontal * camRight) + (Vertical * camForward));
+
+        movePlayer = movePlayer * speed;
+
+        player.transform.LookAt(player.transform.position + movePlayer);
+
+        Vector3 move = new Vector3(Horizontal, 0.0f, Vertical);
+
+        // function for gravity
+        SetGravity();
+
+        // functions
+        playerSkills();
+
+        player.Move(movePlayer * Time.deltaTime);
+    }
+
+    // function for the camera
+    public void camDirection()
+    {
+        // forward direction of main camera
+        camForward = mainCamera.transform.forward;
+        //direction to the right of the main camera
+        camRight = mainCamera.transform.right;
+
+        // y axis set to 0
+        camForward.y = 0;
+        // right relative to y axis set to 0
+        camRight.y = 0;
+
+        // Adjusted vector direction
+        camForward = camForward.normalized;
+        camRight = camRight.normalized;
+    }
+
+    // function Skills
+    public void playerSkills()
+    {
+        if (player.isGrounded && Input.GetButtonDown("Jump"))
         {
-            player.Move(new Vector3(0, gravityValue * jump * Time.deltaTime, 0));
+            fallVelocity = jumpForce;
+            movePlayer.y = fallVelocity;
+        }
+    }
+
+    // function for gravity
+    public void SetGravity()
+    {
+        if (player.isGrounded)
+        {
+            fallVelocity = -gravity * Time.deltaTime;
+            movePlayer.y = fallVelocity;
+        }
+        else
+        {
+            fallVelocity -= gravity * Time.deltaTime;
+            movePlayer.y = fallVelocity;
         }
     }
 }
