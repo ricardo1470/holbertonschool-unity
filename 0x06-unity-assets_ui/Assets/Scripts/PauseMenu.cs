b5b0200bcd24af0144b6e3ba90f9a8d4ca24bac2
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2021-02-17 02:49:37 -0500

    add win canva

---
+++ 0x06-unity-assets_ui/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections.Generic;
+
+public class PauseMenu : MonoBehaviour
+{
+    public Button RestartButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void Pause()
+    {
+        // value keyboard Esc
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            //PauseCanvas.SetActive(true);
+        }
+    }
+
+    public void Resume()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            //PauseCanvas.SetActive(true);
+        }
+    }
+
+    public void Restart()
+    {}
+}
