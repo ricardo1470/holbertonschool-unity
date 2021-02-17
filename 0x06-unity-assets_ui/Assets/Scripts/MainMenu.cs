1e30006d903b160df4e4cfdd8c87498eced5123b
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2021-02-16 21:54:31 -0500

    add scripts

---
+++ 0x06-unity-assets_ui/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+public class MainMenu : MonoBehaviour
+{
+    // level1 button
+    public Button Level01;
+    // level2 button
+    public Button Level02;
+    // level3 button
+    public Button Level03;
+    // options button
+    public Button OptionsButton;
+    // exit button
+    public Button ExitButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Level01.onClick.AddListener(delegate {LevelSelect(0); });
+		Level02.onClick.AddListener(delegate {LevelSelect(1); });
+		Level03.onClick.AddListener(delegate {LevelSelect(2); });
+        OptionsButton.onClick.AddListener(delegate {LevelSelect(4); });
+        ExitButton.onClick.AddListener(() => ExitPlatforns());
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    // function that loads the different levels
+    public void LevelSelect(int level)
+    {
+
+        SceneManager.LoadScene(level);
+    }
+
+    // method that loads the options
+    public void Options()
+    {
+        SceneManager.LoadScene("Options");
+    }
+
+    // method should close the game window
+    // when the Quit button is pressed.
+    public void ExitPlatforns()
+    {
+        Debug.Log("Exited");
+        Application.Quit();
+    }
+}
---
+++ 0x06-unity-assets_ui/Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+public class OptionsMenu : MonoBehaviour
+{
+    // return button
+    public Button BackButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    // function to return to the previous menu
+    public void Back()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
