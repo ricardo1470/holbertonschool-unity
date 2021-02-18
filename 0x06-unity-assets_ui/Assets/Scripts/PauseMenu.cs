4a75d8a8d39e12907e353b5da9fbd8df276c0dc5
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2021-02-18 12:33:13 -0500

    update scripts scape

--- 0x06-unity-assets_ui/Assets/Scripts/PauseMenu.cs
+++ 0x06-unity-assets_ui/Assets/Scripts/PauseMenu.cs
@@ -8,13 +8,47 @@ public class PauseMenu : MonoBehaviour
 {
     //public Button RestartButton;
 
-    [FormerlySerializedAs("PauseCanvas")] public GameObject pauseCanvas;
-    
-    public void Pause()
-    {}
+    public GameObject pauseCanvas;
+    bool paused = false;
+    public Button ResumeButton;
+
+    public void Start()
+    {
+        ResumeButton.onClick.AddListener(delegate
+        {
+            pauseCanvas.SetActive(false);
+            Resume();
+        });
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (paused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    private void Pause()
+    {
+        Time.timeScale = 0;
+        pauseCanvas.SetActive(true);
+        paused = true;
+    }
 
     private void Resume()
-    {}
+    {
+        Time.timeScale = 1;
+        pauseCanvas.SetActive(false);
+        paused = false;
+    }
 
     public void Restart()
     {}
