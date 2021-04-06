defea7f90659b8717c01bfaee7b830d28f535a7e
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2021-04-05 19:32:51 -0500

    create files

---
+++ 0x09-unity_ar_business_card/Assets/Script/Button.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    // GitHub button message
+    public void GitHub()
+    {
+        Debug.Log($"<color=green><b> GitHub Button! </b></color>");
+    }
+    
+    // Linkeind button message
+    public void Linkeind()
+    {
+        Debug.Log($"<color=green><b> Linkeind Button! </b></color>");
+    }
+    
+    // Telegram button message
+    public void Telegram()
+    {
+        Debug.Log($"<color=green><b> Telegram Button! </b></color>");
+    }
+    
+    // Twitter button message
+    public void Twitter()
+    {
+        Debug.Log($"<color=green><b> Twitter Button! </b></color>");
+    }
+    
+    // Email button message
+    public void Email()
+    {
+        Debug.Log($"<color=green><b> Email Button! </b></color>");
+    }
+}
